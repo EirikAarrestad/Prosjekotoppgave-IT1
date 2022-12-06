b15e20e86810d0b18051913a4705179eca84c101
EirikAarrestad <119494327+EirikAarrestad@users.noreply.github.com>
2022-12-06 14:57:06 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+//Deklarer variabler
+const contactContainer = document.querySelector(".wrapper");
+
+//Lager et objekt med tittel og tekst som inneholder informasjon som blir brukt i footeren
+let contactInfo = [
+    {
+        title: "Besøksadresse",
+        underText: "Skien videregående skole",
+    },
+    {
+        title: "Postadresse",
+        underText: "Postboks 2844 | 3702 Skien",
+    },
+    {
+        title: "Telefon",
+        underText: "35 91 80 00",
+    },
+    {
+        title: "E-post",
+        underText: "eksempel@vtfk.no",
+    },
+];
+
+let contactText = "";
+//Lager en "for løkke" for å lage hele grid-containeren med objektes titel og tekst
+for (let i = 0; i < contactInfo.length; i++) {
+    contactText +=
+        "<div id='gridContainer'>" +
+        "<p class='header'>" +
+        contactInfo[i].title +
+        "</p>" +
+        "<p class='text'>" +
+        contactInfo[i].underText +
+        "</p>" +
+        "</div>";
+}
+contactContainer.innerHTML = contactText;
